9263cec2de7a835c44d81f7ee5a5657a3b9f5ab7
Benjamin Caure <bcaure@gmail.com>
2020-02-17 15:48:10 +0100

    :tada: gatsby site

---
+++ gatsby-site/gatsby-config.js
@@ -0,0 +1,24 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+    module.exports = {
+      plugins: [
+        /*
+        * Gatsby's data processing layer begins with “source” plugins. Here we
+        * setup the site to pull data from the "documents" collection in a local
+        * MongoDB instance
+        */
+        {
+          resolve: `gatsby-source-mongodb`,
+          options: {
+            server: { address: 'bmbu7ynqra11rqi-mongodb.services.clever-cloud.com', port: '27017' },
+            auth: { user: 'uepch5uqblw5mad6k1x1', password: 'BN5Ufr4twpbJqZjdshDr'},
+            dbName: 'bmbu7ynqra11rqi',
+            collection: ['cars', 'models', 'brands']
+          },
+        },
+      ],
+    }
---
+++ gatsby-site/src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { graphql } from 'gatsby'
+
+export default () => <div>Hello world!</div>
+
+// export const query = graphql`query {
+//     allMongodbCloudDocuments {
+//       edges {
+//         node {
+//           id
+//           url
+//           name
+//         }
+//       }
+//     }
+//   }`;

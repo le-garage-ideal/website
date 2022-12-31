28f22cfda79f0c3028df44c7b44d84b3bc6a272c
Benjamin Caure <bcaure@gmail.com>
2023-01-01 00:56:33 +0100

    :sparkles: update content types + api tuning

---
+++ strapi-cms/config/plugins.ts
@@ -0,0 +1,12 @@
+const config = ({ env })=>({
+  upload: {
+    config: {
+      providerOptions: {
+        localServer: {
+          maxage: 300000
+        },
+      },
+    },
+  },
+});
+export default config;

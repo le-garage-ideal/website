65ee85642a6154b2a3e7c37d1170f2bc92c2e4cc
Benjamin Caure <bcaure@gmail.com>
2023-09-05 11:20:40 +0200

    :bug: image domain doesn't work on server

--- nextjs-site/next.config.js
+++ nextjs-site/next.config.js
@@ -1,4 +1,8 @@
 const { i18n } = require('./next-i18next.config')
+
+const IMAGE_DOMAIN = Array.from(process.env.NEXT_PUBLIC_STRAPI_BASE_IMG_URL.matchAll(/\/\/([\w\d\.\-_]*)/gi))[0][1];
+console.log("🚀 ~ file: next.config.js:4 ~ IMAGE_DOMAIN:", IMAGE_DOMAIN)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -10,7 +14,7 @@ const nextConfig = {
   },
   i18n,
   images: {
-    domains: ["127.0.0.1"],
+    domains: [IMAGE_DOMAIN],
   },
 }
 

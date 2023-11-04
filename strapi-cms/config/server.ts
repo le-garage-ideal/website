fa321cb0b5a91bcacd4d072fdf475fc90e6837eb
Benjamin Caure <benjamin.caure_ext@michelin.com>
2023-11-04 19:20:01 +0100

    :bug: try to fix prod bug (strapi installed under /admin path)

--- strapi-cms/config/server.ts
+++ strapi-cms/config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'http://0.0.0.0:1337/strapi/'),
   app: {
     keys: env.array('APP_KEYS'),
   },
